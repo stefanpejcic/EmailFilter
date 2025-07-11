afb2d329b0607089cbfae25f93eefc8e2c75fb8b
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2025-07-11 22:25:47 +0200

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,25 @@
+DISPOSABLE_DOMAINS = {
+    "mailinator.com",
+    "10minutemail.com",
+    "trashmail.com"
+}
+
+BLACKLISTED_DOMAINS = {
+    "spamdomain.com"
+}
+
+WHITELISTED_DOMAINS = {
+    "gmail.com",
+    "yahoo.com",
+    "outlook.com"
+}
+
+SPAM_KEYWORDS = [
+    "lottery",
+    "prize",
+    "cash",
+    "offer",
+    "winner",
+    "free",
+    "gift"
+]
