80b736cd809ae0d8d1a60a064b33d55836aa1c6e
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2025-07-13 14:42:04 +0200

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,25 +1,8 @@
-DISPOSABLE_DOMAINS = {
-    "mailinator.com",
-    "10minutemail.com",
-    "trashmail.com"
-}
+def load_list_from_file(filename: str) -> set:
+    with open(filename, 'r') as f:
+        return set(line.strip() for line in f if line.strip())
 
-BLACKLISTED_DOMAINS = {
-    "spamdomain.com"
-}
-
-WHITELISTED_DOMAINS = {
-    "gmail.com",
-    "yahoo.com",
-    "outlook.com"
-}
-
-SPAM_KEYWORDS = [
-    "lottery",
-    "prize",
-    "cash",
-    "offer",
-    "winner",
-    "free",
-    "gift"
-]
+DISPOSABLE_DOMAINS = load_list_from_file('lists/disposable_domains.txt')
+BLACKLISTED_DOMAINS = load_list_from_file('lists/blacklisted_domains.txt')
+WHITELISTED_DOMAINS = load_list_from_file('lists/whitelisted_domains.txt')
+SPAM_KEYWORDS = load_list_from_file('lists/spam_keywords.txt')
